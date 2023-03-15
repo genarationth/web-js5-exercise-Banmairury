61b12aacf640bc0c25659dc8b564d250d13201f5
Teerapong <banmairuruy@gmail.com>
2023-03-15 19:37:12 +0700

    answer

---
+++ script.js
@@ -0,0 +1,61 @@
+//Exercise#1
+
+//Part 1
+const names = ['Sofia','David','Juan'];
+
+//Part 2
+names.push('Sara','Augustin');
+console.log(names);
+
+//Part3
+names.splice(2,0,'Renata');
+names.push('Elena');
+console.log(names);
+
+
+//Exercise #2
+let star = '';
+for(let i = 0;i < 5; i++){
+    star+='*';
+    console.log(star);
+}
+
+//Exercise #3
+let xValue = 5
+let i = 0;
+while( i < xValue){
+    xValue -= 0.5;
+    console.log(xValue)
+}
+
+//Part 2
+let number = 100;
+let j = 1;
+while( j <= number){
+    if(j %2 === 0){
+       console.log(j) 
+    }
+    j++;
+}
+
+//Part 3
+let n = 10;
+let arrNum = [];
+let k = 1;
+while (k <= n){
+    //arrNum.push(k);
+    arrNum = [k];
+    console.log(arrNum);
+     k++;
+}
+
+
+//Part 4
+let numPositive = 19;
+let result = 0;
+let l = 0;
+while (l <= numPositive){
+    result += l;
+    l++
+}
+console.log(result);
